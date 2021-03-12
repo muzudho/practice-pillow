3f52783454bc8ac91d6405a00ac472d5aae62d6f
むずでょ <muzudho1@gmail.com>
2021-03-12 20:47:42 +0900

    RGB

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # practice-pillow
+
 Python/Pillowの練習（＾ｑ＾）
+
+## Refernces
+
+[Python, Pillowで円や四角、直線などの図形を描画](https://note.nkmk.me/python-pillow-imagedraw/)  
---
+++ step1.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageDraw
+
+black = (0, 0, 0)
+red = (0xff, 0x33, 0x66)
+green = (0x66, 0xff, 0x33)
+blue = (0x33, 0x66, 0xff)
+white = (255, 255, 255)
+
+im = Image.new('RGB', (500, 500), white)
+draw = ImageDraw.Draw(im)
+
+# 四角を縦に16個並べる
+# 幅0、高さ0 で、 1x1 の矩形になる。ブラシの太さ1が関係する？
+w = 4
+h = 0
+
+x = 10
+y = 9
+for i in range(0, 16):
+    xx = x
+    draw.rectangle((xx, y, xx+w, y+h), fill=red, outline=None)
+    xx += w+2
+    draw.rectangle((xx, y, xx+w, y+h), fill=green, outline=None)
+    xx += w+2
+    draw.rectangle((xx, y, xx+w, y+h), fill=blue, outline=None)
+
+    # 2px は開けないと、くっついている。 +1 だと隣なので
+    y += h+2
+
+y -= h+3
+xx = x
+draw.text((xx, y), "F", red)
+xx += w+2
+draw.text((xx, y), "F", green)
+xx += w+2
+draw.text((xx, y), "F", blue)
+
+im.save('shared/pillow_imagedraw.png', quality=95)
